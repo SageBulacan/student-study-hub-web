4af9ef7a46f55b1ee769fe485962719bbe2a5dbb
patrick0719 <patrickmercado0719@gmail.com>
2026-09-16 13:54:06 +0800

    Deploy 1.0.9 with visible marker for definitive update test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b9fdd82087bc7531715deb9ef00faff6",
+const RESOURCES = {"flutter_bootstrap.js": "8cc271354d1a27375359e0e27b935cdb",
 "version.json": "5df12a27ce1a0842f793ee0853ac298e",
 "index.html": "164def8a5f993c5f5a9ff5a416e9a7f1",
 "/": "164def8a5f993c5f5a9ff5a416e9a7f1",
-"main.dart.js": "740429cc266986b5f8a2fd796758c0fa",
+"main.dart.js": "b0716b5c0cd5bee8873430ab78dad0ac",
 "sqlite3.wasm": "2e9fc1ccbb9d15199fccf405b0ceee53",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
