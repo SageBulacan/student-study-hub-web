6d4bd884c4a2a7a98a66f23febf2ec673ffeba00
patrick0719 <patrickmercado0719@gmail.com>
2026-09-18 13:50:13 +0800

    Revert to 1.2.14: restore last working PDF zoom config (pre-viewport/touchAction changes)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "46313a9fe2f6afa8d6233702137bc81e",
-"version.json": "4e55f022f252903c9a218674d8274338",
-"index.html": "e21bb46e87d5853ad56c79b700a579e1",
-"/": "e21bb46e87d5853ad56c79b700a579e1",
-"main.dart.js": "d812cd802f8351a1a6588f9f0f673fd3",
+const RESOURCES = {"flutter_bootstrap.js": "95ff2fd496cce77922e24844eef9819c",
+"version.json": "9fe0cd2486ae48aee86bceb7c0241faa",
+"index.html": "1ef86fc3e9216eea76cc2df7a7ec3f95",
+"/": "1ef86fc3e9216eea76cc2df7a7ec3f95",
+"main.dart.js": "140bcf02d9a33e775f2bee2abb125a81",
 "sqlite3.wasm": "2e9fc1ccbb9d15199fccf405b0ceee53",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
